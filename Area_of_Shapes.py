2b561386dc38f53f9b8a93f23c6fcc703b09e922
Alex Lo Storto <lostorto.business@gmail.com>
2023-02-18 10:25:34 +0000

    Add Area_Of_Shapes.py

---
+++ Area_of_Shapes.py
@@ -0,0 +1,41 @@
+PI = 3.14159265359
+ROUND = 2
+
+
+def circleArea(radius):
+    area = round((radius**2*PI), ROUND)
+    print(f"Area: {area}")
+
+
+def circleData(radius):
+    diameter = radius * 2
+    circumference = round(diameter * PI, ROUND)
+    area = round((radius ** 2 * PI), ROUND)
+    print(f"Diameter: {diameter}")
+    print(f"Circumference: {circumference}")
+    print(f"Area: {area}")
+
+
+def calculateDensity(volume, mass):
+    density = round(mass / volume, ROUND)
+    print(f"Density: {density}")
+
+
+def calculateMass(volume, density):
+    mass = round(density * volume, ROUND)
+    print(f"Mass: {mass}")
+
+
+def calculateVolume(mass, density):
+    volume = round(mass / density, ROUND)
+    print(f"Volume: {volume}")
+
+
+def cylinderSurfaceArea(radius, height):
+    surfaceArea = ((radius**2*2) + (2*radius*height))
+    print(f"Surface Area: {round(surfaceArea * PI, ROUND)}")
+    print(f"Surface Area: {round(surfaceArea, ROUND)}π")
+
+
+if __name__ == '__main__':
+    calculateDensity(0.00032, 0.33)
