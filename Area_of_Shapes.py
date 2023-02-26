518804dc0d43fc889675b91186db524802d00b8c
Alex Lo Storto <lostorto.business@gmail.com>
2023-02-26 14:54:55 +0000

    Add log and return option for all functions in Area_of_Shapes.py

--- Area_of_Shapes.py
+++ Area_of_Shapes.py
@@ -2,39 +2,63 @@ PI = 3.14159265359
 ROUND = 2
 
 
-def circleArea(radius):
+def circleArea(radius, log=True):
     area = round((radius**2*PI), ROUND)
-    print(f"Area: {area}")
+
+    if log == True:
+        print(f"Area: {area}")
+
+    return area
 
 
-def circleData(radius):
+def circleData(radius, log=True):
     diameter = radius * 2
     circumference = round(diameter * PI, ROUND)
     area = round((radius ** 2 * PI), ROUND)
-    print(f"Diameter: {diameter}")
-    print(f"Circumference: {circumference}")
-    print(f"Area: {area}")
+
+    if log == True:
+        print(f"Diameter: {diameter}")
+        print(f"Circumference: {circumference}")
+        print(f"Area: {area}")
+
+    return diameter, circumference, area
 
 
-def calculateDensity(volume, mass):
+def calculateDensity(volume, mass, log=True):
     density = round(mass / volume, ROUND)
-    print(f"Density: {density}")
+
+    if log == True:
+        print(f"Density: {density}")
+
+    return density
 
 
-def calculateMass(volume, density):
+def calculateMass(volume, density, log=True):
     mass = round(density * volume, ROUND)
-    print(f"Mass: {mass}")
+
+    if log == True:
+        print(f"Mass: {mass}")
+
+    return mass
 
 
-def calculateVolume(mass, density):
+def calculateVolume(mass, density, log=True):
     volume = round(mass / density, ROUND)
-    print(f"Volume: {volume}")
+
+    if log == True:
+        print(f"Volume: {volume}")
+
+    return volume
 
 
-def cylinderSurfaceArea(radius, height):
+def cylinderSurfaceArea(radius, height, log=True):
     surfaceArea = ((radius**2*2) + (2*radius*height))
-    print(f"Surface Area: {round(surfaceArea * PI, ROUND)}")
-    print(f"Surface Area: {round(surfaceArea, ROUND)}π")
+
+    if log == True:
+        print(f"Surface Area: {round(surfaceArea * PI, ROUND)}")
+        print(f"Surface Area: {round(surfaceArea, ROUND)}π")
+
+    return round(surfaceArea * PI, ROUND), round(surfaceArea, ROUND) + "π"
 
 
 __all__ = [
