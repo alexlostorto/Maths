234e49475ff497ad6a5ef4e535d3bae2b1ae8007
Alex Lo Storto <lostorto.business@gmail.com>
2023-03-11 17:20:24 +0000

    Fix frustumVolume function

--- Volume_of_Shapes.py
+++ Volume_of_Shapes.py
@@ -6,17 +6,18 @@ def frustumVolume(baseShape='rectangle', frustumHeight=1, baseLength=1, baseWidt
     if baseShape.lower() == 'triangle':
         baseArea = 1 / 2 * baseLength * baseWidth
         frustumArea = 1 / 2 * frustumLength * frustumWidth
+        volume = round(frustumHeight / 3 * (baseArea + frustumArea + (baseArea * frustumArea) ** (1 / 2)), ROUND)
     elif baseShape.lower() == 'circle':
-        return round(1 / 3 * PI * frustumHeight * ((frustumLength / 2) ** 2 + (baseLength / 2) ** 2 + ((frustumLength / 2) * (baseLength / 2))), ROUND)
+        volume = round(1 / 3 * PI * frustumHeight * ((frustumLength / 2) ** 2 + (baseLength / 2) ** 2 + ((frustumLength / 2) * (baseLength / 2))), ROUND)
     else:
         baseArea = baseLength * baseWidth
         frustumArea = frustumLength * frustumWidth
+        volume = round(frustumHeight / 3 * (baseArea + frustumArea + (baseArea * frustumArea) ** (1 / 2)), ROUND)
 
     if log == True:
-        print(round(frustumHeight / 3 * (baseArea + frustumArea +
-              (baseArea * frustumArea) ** (1 / 2)), ROUND))
+        print(f"Volume: {volume}")
 
-    return round(frustumHeight / 3 * (baseArea + frustumArea + (baseArea * frustumArea) ** (1 / 2)), ROUND)
+    return volume
 
 
 __all__ = [
